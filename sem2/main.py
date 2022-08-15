d8c788abd3704cceca0d545045c58eea06a06c1e
WILLIAMKATH5 <williamkath5@hotmail.com>
2022-08-14 22:43:27 -0500

    1 ejercicio de python
    Signed-off-by: WILLIAMKATH5 <Juli@strid74170>

--- sem2/main.py
+++ sem2/main.py
@@ -1,4 +1,16 @@
 # Python Semana 2
-nombre= input("Por favor, ingresa tu nombre: ")
-print("Hola", nombre,", Cómo estás?")
+nombre = input("Por favor, ingresa tu nombre: ")
+print("\nHola", nombre, ", \nCómo estás?")
 
+# Variables numéricas:
+# Índice de masa corporal
+# es igual a la masa sobre el cuadrado de la altura
+
+altura=1.84
+masa=80
+
+# Variable de texto:
+nombre="William"
+
+IMC = masa/(altura**2)
+print(nombre, ", Tu índice de masa corporal es de ", IMC)
